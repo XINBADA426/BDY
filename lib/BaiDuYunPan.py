38965331e7fba36e901e4a5a7f92b1d64a8064fa
Ming Jia <cbren@visionmedicals.com>
2022-07-26 10:00:39 +0800

    Init

---
+++ lib/BaiDuYunPan.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Chaobo Ren
+# @Date:   2022/7/18 9:07
+# @Last Modified by:   Ming
+# @Last Modified time: 2022/7/18 9:07
+import hashlib
+import json
+import logging
+import sys
+from pathlib import Path
+from urllib.parse import urlencode
+
+import requests
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+BASEPATH = Path(__file__).absolute()
+# 百度云API
+sys.path.append(str(BASEPATH))
+import openapi_client
+from openapi_client.api import userinfo_api
+from openapi_client.api import fileupload_api
+
+
+class BDYP(object):
+    """
+    百度网盘API的进一步封装
+    """
+
+    def __init__(self, thread=4):
+        """
+        初始化百度网盘对象
+        """
+        # 一些基本信息
+        self._client_id = "EyUURtW1VbHkibk4nGGOUk4c7B9Nxnse"
+        self._redirect_uri = "oob"
+        self._device_id = "26708729"
+        self._SecretKey = "w88HIgI8u7bR4gFNwcdjgvuBxDbKBPTL"
+
+        # 会话信息
+        self.client = openapi_client.ApiClient(pool_threads=thread)
+
+        self._f_token = Path.home().joinpath(".BDY.json")
+        self.login()
+        self.quote()
+
+    def login(self):
+        """
+        登陆网盘
+        """
+        if self._f_token.exists():
+            self._parse_token_file()
+        else:
+            self._authorize()
+            self._code2token()
+
+    def _authorize(self):
+        """
+        获取授权码
+        """
+        url = "http://openapi.baidu.com/oauth/2.0/authorize?"
+        param = {"response_type": "code",
+                 "client_id": self._client_id,
+                 "redirect_uri": self._redirect_uri,
+                 "scope": "basic netdisk",
+                 "device_id": self._device_id}
+        full_url = f"{url}{urlencode(param)}"
+        logging.info(f"Open the URL: {full_url}")
+        self._authorize_code = input(f"Parse the Authorization Code here within 10 minutes: ")
+
+    def _code2token(self):
+        """
+        通过授权码获取Access Token
+        """
+        url = "https://openapi.baidu.com/oauth/2.0/token?"
+        param = {"grant_type": "authorization_code",
+                 "code": self._authorize_code,
+                 "client_id": self._client_id,
+                 "client_secret": self._SecretKey,
+                 "redirect_uri": self._redirect_uri}
+        res = requests.get(f"{url}{urlencode(param)}")
+        logging.debug(res.text)
+        self._token = json.loads(res.text)["access_token"]
+        logging.info(f"Save the token info to {self._f_token}")
+        with open(self._f_token, 'w') as OUT:
+            print(res.text, file=OUT)
+
+    def _parse_token_file(self):
+        """
+        解析储存授权信息的文件
+        """
+        logging.info(f"Parse the token from file: {self._f_token}")
+        self._token = json.load(open(self._f_token))["access_token"]
+
+    def quote(self):
+        """
+        获取用户信息
+        """
+        api_instance = userinfo_api.UserinfoApi(self.client)
+
+        try:
+            userinfo = api_instance.xpannasuinfo(self._token)
+            logging.info(f"用户名：{userinfo['baidu_name']}")
+            logging.info(f"授权状态：{userinfo['errmsg']}")
+        except openapi_client.ApiException as e:
+            logging.error(f"Can not get user info: {e}")
+            sys.exit(-1)
+
+    def upload(self, local, remote):
+        """
+        上传文件
+
+        TODO: 1. 添加多文件支持
+              2. 添加每一步骤的结果验证
+
+        :param local: 本地文件路径
+        :param remote: 远程文件路径
+        """
+        local = Path(local).absolute()
+        handle = open(local, 'rb')
+        f_size = local.stat().st_size
+        f_md5 = f"[\"{hashlib.md5(open(local, 'rb').read()).hexdigest()}\"]"
+        remote = f"/apps/BDY/{remote}"
+        logging.debug(f_md5)
+        obj = fileupload_api.FileuploadApi(self.client)
+
+        # 预上传
+        try:
+            precreate_response = obj.xpanfileprecreate(self._token, remote, 0, f_size, 1, f_md5, rtype=3)
+        except BaseException as e:
+            logging.error(e)
+
+        # 上传
+        try:
+            upload_response = obj.pcssuperfile2(self._token, '0', remote, precreate_response['uploadid'], "tmpfile",
+                                                file=handle)
+        except BaseException as e:
+            logging.error(e)
+
+        # 创建
+        try:
+            create_response = obj.xpanfilecreate(self._token, remote, 0, f_size, precreate_response['uploadid'], f_md5,
+                                                 rtype=3)
+        except BaseException as e:
+            logging.error(e)
---
+++ main.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Chaobo Ren
+# @Date:   2022/7/15 9:49
+# @Last Modified by:   Ming
+# @Last Modified time: 2022/7/15 9:49
+import logging
+import sys
+from pathlib import Path
+
+BASEPATH = Path(__file__)
+sys.path.append(str(BASEPATH.absolute().parent.joinpath("lib")))
+from BaiDuYunPan import BDYP
+import click
+
+#### Some Functions and global variable
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+__version__ = 'v0.0.1'
+
+
+########################
+
+
+@click.group(context_settings=dict(help_option_names=['-h', '--help']))
+@click.version_option(version=__version__)
+def cli():
+    """
+    Tools for deal with fasta file
+    """
+    pass
+
+
+@click.command()
+@click.option('--local',
+              type=click.Path(),
+              required=True,
+              help='The local file to upload')
+@click.option('--remote',
+              type=click.Path(),
+              required=True,
+              help='The remote path')
+@click.option('-t', '--thread',
+              default=8,
+              type=int,
+              help='The thread to use')
+def upload(local, remote, thread):
+    """
+    文件上传工具
+    """
+    session = BDYP(thread=thread)
+    session.upload(local, remote)
+
+
+cli.add_command(upload)
+
+#### Main
+if __name__ == "__main__":
+    cli()
